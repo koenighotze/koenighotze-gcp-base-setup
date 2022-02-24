b6d33fde8dec1a62c850765c39f9dadef1c963dc
David Schmitz <davidschmitz@posteo.de>
2022-02-24 17:16:02 +0100

    Activate APIs for baseline

--- baseline-project.tf
+++ baseline-project.tf
@@ -6,8 +6,8 @@ locals {
 }
 
 resource "google_project_service" "baseline_project_services" {
-  for_each                   = toset(locals.activate_apis)
-  project                    = google_project.project_baseline.project_id
+  for_each                   = toset(local.activate_apis)
+  project                    = data.google_project.project_baseline.project_id
   service                    = each.value
   disable_on_destroy         = true
   disable_dependent_services = true
