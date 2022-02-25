a568dae3fc7770d4395b9449e21b008765fa80e1
David Schmitz <davidschmitz@posteo.de>
2022-02-25 13:13:14 +0100

    Fix network setup / checkov findings

--- modules/project/main.tf
+++ modules/project/main.tf
@@ -10,6 +10,7 @@ resource "google_project" "project" {
   name       = var.project_name
   project_id = var.project_id
   org_id     = ""
+  auto_create_network   = false
 }
 
 resource "google_project_service" "common_project_services" {
@@ -18,4 +19,17 @@ resource "google_project_service" "common_project_services" {
   service                    = each.value
   disable_on_destroy         = true
   disable_dependent_services = true
+}
+
+resource "google_project_iam_audit_config" "audit" {
+  project = google_project.project.id
+  service = "allServices"
+
+  audit_log_config {
+    log_type = "ADMIN_READ"
+  }
+
+  audit_log_config {
+    log_type = "DATA_READ"
+  }
 }
