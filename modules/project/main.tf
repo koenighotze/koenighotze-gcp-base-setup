47ee7fd6f82cea5349b6921d87b72bf9034241b9
David Schmitz <davidschmitz@posteo.de>
2022-02-28 10:32:16 +0100

    SA does not need to be an admin

--- modules/project/main.tf
+++ modules/project/main.tf
@@ -68,7 +68,7 @@ resource "google_project_iam_binding" "iam_binding_project" {
   ]
 }
 
-esource "google_service_account_iam_member" "gce-default-account-iam" {
+resource "google_service_account_iam_member" "gce-default-account-iam" {
   service_account_id = data.google_compute_default_service_account.default.name
   role               = "roles/iam.serviceAccountUser"
   member             = "serviceAccount:${google_service_account.sa.email}"
