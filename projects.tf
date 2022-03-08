56d9de8da1f60c062986c701b973c9c96cf408fa
David Schmitz <davidschmitz@posteo.de>
2022-03-08 09:46:40 +0100

    Allow unpinned modules

--- projects.tf
+++ projects.tf
@@ -6,6 +6,7 @@ locals {
 
 module "koenighotze_baseline" {
   for_each = toset(local.project_names)
+  # tflint-ignore: terraform_module_pinned_source
   source   = "github.com/koenighotze/gcp-tf-modules/infrastructure-project"
 
   project_id         = "${each.value}-${var.project_postfix}"
