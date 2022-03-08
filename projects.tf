3db45e61ff4014ae9a51c8199496073378e5eb5b
David Schmitz <davidschmitz@posteo.de>
2022-03-08 09:49:55 +0100

    Remove formatting error

--- projects.tf
+++ projects.tf
@@ -7,7 +7,7 @@ locals {
 module "koenighotze_baseline" {
   for_each = toset(local.project_names)
   # tflint-ignore: terraform_module_pinned_source
-  source   = "github.com/koenighotze/gcp-tf-modules/infrastructure-project"
+  source = "github.com/koenighotze/gcp-tf-modules/infrastructure-project"
 
   project_id         = "${each.value}-${var.project_postfix}"
   project_name       = each.value
