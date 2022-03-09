62573b53fb4fff070b16f0bc6034b65be8c57a0c
David Schmitz <davidschmitz@posteo.de>
2022-03-09 10:26:29 +0100

    Fix formatting

--- projects.tf
+++ projects.tf
@@ -9,9 +9,9 @@ module "koenighotze_baseline" {
   # tflint-ignore: terraform_module_pinned_source
   source = "github.com/koenighotze/gcp-tf-modules/infrastructure-project"
 
-  project_id         = "${each.value}-${var.project_postfix}"
-  project_name       = each.value
-  github_admin_token = var.github_admin_token
+  project_id                      = "${each.value}-${var.project_postfix}"
+  project_name                    = each.value
+  github_admin_token              = var.github_admin_token
   workload_identity_provider_name = var.workload_identity_provider_name
-  workload_identity_pool_id = var.workload_identity_pool_id
+  workload_identity_pool_id       = var.workload_identity_pool_id
 }
