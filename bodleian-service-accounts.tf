b0f9cc41e2779088fbbe5e162750c4aa5fc1cd1f
David Schmitz <davidschmitz@posteo.de>
2022-03-14 11:24:39 +0100

    Permissions are managed centrally

--- bodleian-service-accounts.tf
+++ bodleian-service-accounts.tf
@@ -37,3 +37,18 @@ module "bodleian_service_deployer_service_account" {
   description = "This service account handles deployments"
   project_id  = local.project_id
 }
+
+# Allow repository to use deployer service account
+resource "google_service_account_iam_binding" "workload_identity_sa_binding" {
+  # this list must contain all repositories and their respective service account
+  for_each = {
+    module.bodleian_service_deployer_service_account.service_account_id = "koenighotze/bodleian-service-tmp"
+  }
+
+  service_account_id = each.key
+  role               = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/${each.value}",
+  ]
+}
