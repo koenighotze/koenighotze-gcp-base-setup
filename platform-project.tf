2b135f908431eec6bbf29918cdfe53287732c256
David Schmitz <davidschmitz@posteo.de>
2022-03-14 14:55:17 +0100

    Add container registry

--- platform-project.tf
+++ platform-project.tf
@@ -5,9 +5,8 @@ resource "google_container_registry" "container_registry" {
 
 #tfsec:ignore:google-iam-no-privileged-service-accounts
 resource "google_storage_bucket_iam_binding" "bodleian_service_deployer_gcr_iam_binding" {
-  project = data.google_project.platform_project
-  bucket  = google_container_registry.container_registry.id
-  role    = "roles/storage.objectAdmin"
+  bucket = google_container_registry.container_registry.id
+  role   = "roles/storage.objectAdmin"
   members = [
     "serviceAccount:${module.bodleian_service_deployer_service_account.service_email}"
   ]
