c5066c60ecdf145edd97fd260101397f0edc6b10
David Schmitz <davidschmitz@posteo.de>
2022-03-15 10:44:33 +0100

    debugging

--- bodleian-infrastructure-service-account.tf
+++ bodleian-infrastructure-service-account.tf
@@ -11,7 +11,7 @@ locals {
 }
 
 resource "google_service_account" "bodleian_infrastructure_service_account" {
-  project      = bodleian_project_id
+  project      = local.bodleian_project_id
   account_id   = "infra-setup-sa"
   display_name = "Infrastructure Setup Service Account"
   description  = "Service account for infrastructure activities on this project"
@@ -21,10 +21,10 @@ resource "google_service_account" "bodleian_infrastructure_service_account" {
 #tfsec:ignore:google-iam-no-privileged-service-accounts
 resource "google_project_iam_binding" "bodleian_infrastructure_iam_binding_project" {
   for_each = toset(local.project_roles)
-  project  = bodleian_project_id
+  project  = local.bodleian_project_id
   role     = each.value
 
   members = [
-    "serviceAccount:${google_service_account.service_account.email}"
+    "serviceAccount:${google_service_account.bodleian_infrastructure_service_account.email}"
   ]
 }
