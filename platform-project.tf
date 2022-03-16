a95fc9eef6f3dd4af32ba276fcc8baef9718e9a5
David Schmitz <davidschmitz@posteo.de>
2022-03-16 07:54:11 +0100

    debugging

--- platform-project.tf
+++ platform-project.tf
@@ -16,21 +16,44 @@ resource "google_container_registry" "container_registry" {
   location = "EU"
 }
 
-#tfsec:ignore:google-iam-no-privileged-service-accounts
-resource "google_storage_bucket_iam_binding" "bodleian_service_deployer_gcr_iam_binding" {
-  for_each = toset([
-    "roles/storage.legacyBucketReader",
-    "roles/storage.objectAdmin"
-  ])
+data "google_iam_policy" "deployer_policy" {
+  binding {
+    role = "roles/storage.legacyBucketWriter"
 
-  bucket = google_container_registry.container_registry.id
-  role   = each.value
-  members = [
-    "serviceAccount:${module.bodleian_service_deployer_service_account.service_account_email}"
-  ]
+    members = [
+      "serviceAccount:${module.bodleian_service_deployer_service_account.service_account_email}"
+    ]
+  }
+
+  binding {
+    role = "roles/storage.legacyBucketReader)"
+
+    members = [
+      "serviceAccount:${module.bodleian_service_deployer_service_account.service_account_email}"
+    ]
+  }
 }
 
-# TODO set ACL on bucket!
+resource "google_storage_bucket_iam_policy" "policy" {
+  bucket      = google_container_registry.container_registry.id
+  policy_data = data.google_iam_policy.deployer_policy.policy_data
+}
+
+
+#tfsec:ignore:google-iam-no-privileged-service-accounts
+# resource "google_storage_bucket_iam_binding" "bodleian_service_deployer_gcr_iam_binding" {
+#   for_each = toset([
+#     "roles/storage.legacyBucketWriter",
+#     "roles/storage.objectAdmin"
+#   ])
+
+#   bucket = google_container_registry.container_registry.id
+#   role   = each.value
+#   members = [
+#     "serviceAccount:${module.bodleian_service_deployer_service_account.service_account_email}"
+#   ]
+# }
+
 # get bucket via data
 # set acl
 
