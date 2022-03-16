1b7659b0e8f0d166e0be4d9674367ec36dafb5de
David Schmitz <davidschmitz@posteo.de>
2022-03-16 14:33:07 +0100

    Corrected registry name

--- outputs.tf
+++ outputs.tf
@@ -11,6 +11,6 @@ output "bodleian_service_deployer_service_account_email" {
 }
 
 output "docker_registry" {
-  value = "${google_artifact_registry_repository.docker.location}-docker.pkg.dev/${data.google_project.platform_project.project_id}"
+  value = "${google_artifact_registry_repository.docker.location}-docker.pkg.dev/${data.google_project.platform_project.project_id}/${google_artifact_registry_repository.docker.name}"
 }
 
