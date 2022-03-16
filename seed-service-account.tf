3fa88f5265a3b51f6908aa78ea5959cd21860eb8
David Schmitz <davidschmitz@posteo.de>
2022-03-16 10:19:47 +0100

    Seed must be storage admin on platform

---
+++ seed-service-account.tf
@@ -0,0 +1,13 @@
+# This SA needs to be able to do some privileged work
+#tfsec:ignore:google-iam-no-privileged-service-accounts
+resource "google_project_iam_binding" "seed_platform_iam_binding_project" {
+  for_each = toset([
+    "roles/storage.admin"
+  ])
+  project = data.google_project.platform_project.project_id
+  role    = each.value
+
+  members = [
+    "serviceAccount:${data.google_service_account.seed_sa.email}"
+  ]
+}
