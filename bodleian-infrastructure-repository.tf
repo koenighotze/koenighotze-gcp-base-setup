7913f874d4f8f5812e47c6b6347e6c00feaa3ed1
David Schmitz <davidschmitz@posteo.de>
2022-03-21 12:58:32 +0100

    WIP

--- bodleian-infrastructure-repository.tf
+++ bodleian-infrastructure-repository.tf
@@ -1,16 +1,5 @@
-locals {
-  project_roles = [
-    "roles/logging.logWriter",
-    "roles/viewer"
-    # "roles/iam.serviceAccountAdmin"
-    # "roles/iam.securityAdmin"
-  ]
-
-  bodleian_project_id = data.google_project.bodleian_project.project_id
-}
-
 resource "google_service_account" "bodleian_infrastructure_service_account" {
-  project      = local.bodleian_project_id
+  project      = data.google_project.bodleian_project.project_id
   account_id   = "infra-setup-sa"
   display_name = "Infrastructure Setup Service Account"
   description  = "Service account for infrastructure activities on this project"
@@ -19,8 +8,13 @@ resource "google_service_account" "bodleian_infrastructure_service_account" {
 # This SA needs to be able to do some privileged work
 #tfsec:ignore:google-iam-no-privileged-service-accounts
 resource "google_project_iam_binding" "bodleian_infrastructure_iam_binding_project" {
-  for_each = toset(local.project_roles)
-  project  = local.bodleian_project_id
+  for_each = toset(project_roles = [
+    "roles/logging.logWriter",
+    "roles/viewer"
+    # "roles/iam.serviceAccountAdmin"
+    # "roles/iam.securityAdmin"
+  ])
+  project  = data.google_project.bodleian_project.project_id
   role     = each.value
 
   members = [
