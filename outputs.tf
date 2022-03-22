b5030e7ee3abb071d98e130a2efee07e361df155
David Schmitz <davidschmitz@posteo.de>
2022-03-22 13:21:15 +0100

    TF treats status as a 1 element list

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,7 @@ output "bodleian_infrastructure_service_account_email" {
   value = google_service_account.bodleian_infrastructure_service_account.email
 }
 
-output "backend_deployer_service_account_email" {
+output "bodleian_backend_deployer_service_account_email" {
   value = module.backend_deployer_sa.email
 }
 
@@ -18,3 +18,10 @@ output "bodleian_backend_service_service_account_email" {
   value = google_service_account.backend_service_sa.email
 }
 
+output "bodleian_backend_repository_full_name" {
+  value = module.backend_repository.full_name
+}
+
+output "bodleian_infrastructure_repository_full_name" {
+  value = module.bodleian_project.github_repository_full_name
+}
