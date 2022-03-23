904466bad412fbb9a04f65269183d598e7abf131
David Schmitz <davidschmitz@posteo.de>
2022-03-23 14:30:47 +0100

    Add platform

--- platform.tf
+++ platform.tf
@@ -1,8 +1,8 @@
-# module "platform" {
-#   source = "./platform"
+module "platform" {
+  source = "./platform"
 
-#   admin_sa_email      = var.seed_sa_email
-#   project_postfix     = var.project_postfix
-#   artifact_reader_sas = []
-#   artifact_writer_sas = []
-# }
+  admin_sa_email      = var.seed_sa_email
+  project_postfix     = var.project_postfix
+  artifact_reader_sas = []
+  artifact_writer_sas = []
+}
--- seed/billing-account-iam.tf
+++
@@ -1,5 +0,0 @@
-resource "google_billing_account_iam_member" "billing_account_admins" {
-  billing_account_id = var.billing_account_id
-  role               = "roles/billing.admin"
-  member             = "serviceAccount:${data.google_service_account.seed_sa.email}"
-}
