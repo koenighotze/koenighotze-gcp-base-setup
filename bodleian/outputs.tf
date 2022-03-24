fe0955a1c53343c67be2ec4d45d8212ad6bb7bc7
David Schmitz <davidschmitz@posteo.de>
2022-03-24 10:11:23 +0100

    Setup backend repo

--- bodleian/outputs.tf
+++ bodleian/outputs.tf
@@ -6,20 +6,16 @@ output "infrastructure_repository_full_name" {
   value = module.bodleian_project.github_repository_full_name
 }
 
-# output "bodleian_backend_deployer_service_account_email" {
-#   value = module.backend_deployer_sa.email
-# }
+output "backend_deployer_service_account_email" {
+  value = module.backend_deployer_sa.email
+}
 
-# output "container_registry" {
-#   value = "${google_artifact_registry_repository.docker.location}-docker.pkg.dev/${data.google_project.platform_project.project_id}/${google_artifact_registry_repository.docker.name}"
-# }
+output "backend_service_service_account_email" {
+  value = google_service_account.backend_service_sa.email
+}
 
-# output "bodleian_backend_service_service_account_email" {
-#   value = google_service_account.backend_service_sa.email
-# }
-
-# output "bodleian_backend_repository_full_name" {
-#   value = module.backend_repository.full_name
-# }
+output "backend_repository_full_name" {
+  value = module.backend_repository.full_name
+}
 
 
--- bodleian/variables.tf
+++ bodleian/variables.tf
@@ -1,23 +1,3 @@
-# variable "billing_account_id" {
-#   type        = string
-#   sensitive   = true
-#   description = "The id of the billing account"
-# }
-
-# variable "seed_sa_email_address" {
-#   type        = string
-#   sensitive   = true
-#   description = "Email address of the service account"
-# }
-
-# variable "region" {
-#   default = "europe-west3"
-# }
-
-# variable "environment" {
-#   default = "dev"
-# }
-
 variable "github_admin_token" {
   type        = string
   sensitive   = true
