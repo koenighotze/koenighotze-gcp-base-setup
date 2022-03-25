a924d4a6b19480e5a092bfa22870445b53a9f5d3
David Schmitz <davidschmitz@posteo.de>
2022-03-25 14:14:01 +0100

    WIP

--- artifacts-repository-iam.tf
+++ artifacts-repository-iam.tf
@@ -9,23 +9,23 @@ locals {
 }
 
 
-#checkov:skip=CKV_GCP_84:We are fine with Google's keys
-resource "google_artifact_registry_repository_iam_binding" "service_deployer_artifact_registry_readers" {
-  provider = google-beta
-  project  = module.platform.project_id
+# #checkov:skip=CKV_GCP_84:We are fine with Google's keys
+# resource "google_artifact_registry_repository_iam_binding" "service_deployer_artifact_registry_readers" {
+#   provider = google-beta
+#   project  = module.platform.project_id
 
-  location   = module.platform.location
-  repository = module.platform.repository
-  role       = "roles/artifactregistry.reader"
-  members    = [for sa in local.artifact_reader_sas : "serviceAccount:${sa}"]
-}
+#   location   = module.platform.location
+#   repository = module.platform.repository
+#   role       = "roles/artifactregistry.reader"
+#   members    = [for sa in local.artifact_reader_sas : "serviceAccount:${sa}"]
+# }
 
-resource "google_artifact_registry_repository_iam_binding" "service_deployer_artifact_registry_writers" {
-  provider = google-beta
-  project  = module.platform.project_id
+# resource "google_artifact_registry_repository_iam_binding" "service_deployer_artifact_registry_writers" {
+#   provider = google-beta
+#   project  = module.platform.project_id
 
-  location   = module.platform.location
-  repository = module.platform.repository
-  role       = "roles/artifactregistry.writer"
-  members    = [for sa in local.artifact_writer_sas : "serviceAccount:${sa}"]
-}
+#   location   = module.platform.location
+#   repository = module.platform.repository
+#   role       = "roles/artifactregistry.writer"
+#   members    = [for sa in local.artifact_writer_sas : "serviceAccount:${sa}"]
+# }
