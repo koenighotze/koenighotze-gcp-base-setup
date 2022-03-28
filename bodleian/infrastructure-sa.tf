118fd7bacc69d2f013abd4567e5b0ccbc47404ea
David Schmitz <davidschmitz@posteo.de>
2022-03-28 08:52:52 +0200

    WIP

--- bodleian/infrastructure-sa.tf
+++ bodleian/infrastructure-sa.tf
@@ -1,5 +1,5 @@
 resource "google_service_account" "bodleian_infrastructure_service_account" {
-  project      = data.google_project.bodleian_project.project_id
+  project      = data.google_project.project.project_id
   account_id   = "infra-setup-sa"
   display_name = "Infrastructure Setup Service Account"
   description  = "Service account for infrastructure activities on this project"
@@ -14,7 +14,7 @@ resource "google_project_iam_binding" "bodleian_infrastructure_iam_binding_proje
     "roles/run.admin",
     "roles/viewer"
   ])
-  project = data.google_project.bodleian_project.project_id
+  project = data.google_project.project.project_id
   role    = each.value
 
   members = [
