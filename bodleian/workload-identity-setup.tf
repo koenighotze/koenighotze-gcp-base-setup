83f9878c5ff5f04789ae5df71bb3e0bfe0c090b9
David Schmitz <davidschmitz@posteo.de>
2022-03-28 16:31:53 +0200

    WIP

--- bodleian/workload-identity-setup.tf
+++ bodleian/workload-identity-setup.tf
@@ -1,9 +1,8 @@
-# # Allow repository to use deployer service account
-# resource "google_service_account_iam_binding" "bodleian_service_deployer_workload_identity_sa_binding" {
-#   service_account_id = module.backend_deployer_sa.name
-#   role               = "roles/iam.workloadIdentityUser"
+resource "google_service_account_iam_binding" "workload_identity_sa_binding" {
+  service_account_id = module.backend_deployer_sa.id
+  role               = "roles/iam.workloadIdentityUser"
 
-#   members = [
-#     "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/${module.backend_repository.full_name}",
-#   ]
-# }
+  members = [
+    "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/${module.bodleian_project.github_repository_full_name}",
+  ]
+}
