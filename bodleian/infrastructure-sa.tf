3548c2c36873419a2e409122da67a867f103b826
David Schmitz <davidschmitz@posteo.de>
2022-03-30 09:56:43 +0200

    Drop prefix on SAs

--- bodleian/infrastructure-sa.tf
+++ bodleian/infrastructure-sa.tf
@@ -11,12 +11,20 @@ resource "google_project_iam_binding" "bodleian_infrastructure_iam_binding_proje
     "roles/logging.logWriter",
     "roles/iam.serviceAccountViewer",
     "roles/run.admin",
-    "roles/viewer",
-    "roles/iam.serviceAccountUser"
+    "roles/viewer"
   ])
 
   project = data.google_project.project.project_id
   role    = each.key
+
+  members = [
+    "serviceAccount:${google_service_account.bodleian_infrastructure_service_account.email}"
+  ]
+}
+
+resource "google_service_account_iam_binding" "backend_service_sa_user_iam_binding_serviceaccountuser" {
+  service_account_id = google_service_account.backend_service_sa.name
+  role               = "roles/iam.serviceAccountUser"
 
   members = [
     "serviceAccount:${google_service_account.bodleian_infrastructure_service_account.email}"
