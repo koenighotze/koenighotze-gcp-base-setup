833f96c1f28a01a1f4c5ffee713c17a6975f2f0d
David Schmitz <davidschmitz@posteo.de>
2022-04-01 11:01:13 +0200

    Import

--- artifacts-repository-iam.tf
+++ artifacts-repository-iam.tf
@@ -1,31 +1,29 @@
 locals {
   artifact_reader_sas = [
-    # module.bodleian.backend_deployer_service_account_email
+    module.bodleian.backend_deployer_service_account_email
   ]
 
   artifact_writer_sas = [
-    # module.bodleian.backend_deployer_service_account_email
+    module.bodleian.backend_deployer_service_account_email
   ]
 }
 
+resource "google_artifact_registry_repository_iam_binding" "service_deployer_artifact_registry_readers" {
+  provider = google-beta
+  project  = module.platform.project_id
 
-# #checkov:skip=CKV_GCP_84:We are fine with Google's keys
-# resource "google_artifact_registry_repository_iam_binding" "service_deployer_artifact_registry_readers" {
-#   provider = google-beta
-#   project  = module.platform.project_id
+  location   = module.platform.location
+  repository = module.platform.repository
+  role       = "roles/artifactregistry.reader"
+  members    = [for sa in local.artifact_reader_sas : "serviceAccount:${sa}"]
+}
 
-#   location   = module.platform.location
-#   repository = module.platform.repository
-#   role       = "roles/artifactregistry.reader"
-#   members    = [for sa in local.artifact_reader_sas : "serviceAccount:${sa}"]
-# }
+resource "google_artifact_registry_repository_iam_binding" "service_deployer_artifact_registry_writers" {
+  provider = google-beta
+  project  = module.platform.project_id
 
-# resource "google_artifact_registry_repository_iam_binding" "service_deployer_artifact_registry_writers" {
-#   provider = google-beta
-#   project  = module.platform.project_id
-
-#   location   = module.platform.location
-#   repository = module.platform.repository
-#   role       = "roles/artifactregistry.writer"
-#   members    = [for sa in local.artifact_writer_sas : "serviceAccount:${sa}"]
-# }
+  location   = module.platform.location
+  repository = module.platform.repository
+  role       = "roles/artifactregistry.writer"
+  members    = [for sa in local.artifact_writer_sas : "serviceAccount:${sa}"]
+}
