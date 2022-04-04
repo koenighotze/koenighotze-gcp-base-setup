7597c4e881cca32803dd63d449cf174db209f652
David Schmitz <davidschmitz@posteo.de>
2022-04-04 10:34:42 +0200

    Print identity token

--- bodleian/workload-identity-setup.tf
+++ bodleian/workload-identity-setup.tf
@@ -1,18 +1,10 @@
-locals {
-  repositories = [
-    "koenighotze/infrastructure-${data.google_project.project.project_id}",
-    "koenighotze/bodleian-service"
-  ]
-}
-
 resource "google_service_account_iam_binding" "workload_identity_sa_binding" {
-  for_each = toset(local.repositories)
-
   service_account_id = module.backend_deployer_sa.id
   role               = "roles/iam.workloadIdentityUser"
 
   members = [
-    "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/${each.key}"
+    "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/koenighotze/infrastructure-${data.google_project.project.project_id}",
+    "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/koenighotze/bodleian-service"
   ]
 }
 
