ba33e03a8031c9b796685cca838a9ae20ac4a962
David Schmitz <davidschmitz@posteo.de>
2022-04-06 14:23:14 +0200

    Setup artifacts iam

--- artifacts-repository-iam.tf
+++ artifacts-repository-iam.tf
@@ -1,13 +1,35 @@
-# locals {
-#   artifact_reader_sas = [
-#     module.bodleian.backend_deployer_sa_email,
-#     module.bodleian.backend_service_sa_email
-#   ]
+locals {
+  artifact_reader_sas = [
+    module.bodleian.backend_deployer_sa_email,
+    module.bodleian.backend_service_sa_email
+  ]
 
-#   artifact_writer_sas = [
-#     module.bodleian.backend_deployer_sa_email
-#   ]
-# }
+  artifact_writer_sas = [
+    module.bodleian.backend_deployer_sa_email
+  ]
+}
+
+resource "google_artifact_registry_repository_iam_member" "member" {
+  for_each = to_set(local.artifact_reader_sas)
+
+  provider   = google-beta
+  project    = module.platform.project_id
+  location   = module.platform.location
+  repository = module.platform.repository
+  role       = "roles/artifactregistry.reader"
+  member     = "user:${each.key}"
+}
+
+resource "google_artifact_registry_repository_iam_member" "member" {
+  for_each = to_set(local.artifact_writer_sas)
+
+  provider   = google-beta
+  project    = module.platform.project_id
+  location   = module.platform.location
+  repository = module.platform.repository
+  role       = "roles/artifactregistry.writer"
+  member     = "user:${each.key}"
+}
 
 # resource "google_artifact_registry_repository_iam_binding" "service_deployer_artifact_registry_readers" {
 #   provider = google-beta
@@ -15,19 +37,19 @@
 
 #   location   = module.platform.location
 #   repository = module.platform.repository
-#   role       = "roles/artifactregistry.writer"
+#   #   role       = "roles/artifactregistry.admin"
 #   # role       = "roles/artifactregistry.admin"
-#   # role    = "roles/artifactregistry.reader"
+#   role    = "roles/artifactregistry.reader"
 #   members = [for sa in local.artifact_reader_sas : "serviceAccount:${sa}"]
 # }
 
-# # resource "google_artifact_registry_repository_iam_binding" "service_deployer_artifact_registry_writers" {
-# #   provider = google-beta
-# #   project  = module.platform.project_id
+# resource "google_artifact_registry_repository_iam_binding" "service_deployer_artifact_registry_writers" {
+#   provider = google-beta
+#   project  = module.platform.project_id
 
-# #   location   = module.platform.location
-# #   repository = module.platform.repository
-# #   role       = "roles/artifactregistry.admin"
-# #   # role    = "roles/artifactregistry.writer"
-# #   members = [for sa in local.artifact_writer_sas : "serviceAccount:${sa}"]
-# # }
+#   location   = module.platform.location
+#   repository = module.platform.repository
+#   #   role       = "roles/artifactregistry.admin"
+#   role    = "roles/artifactregistry.writer"
+#   members = [for sa in local.artifact_writer_sas : "serviceAccount:${sa}"]
+# }
