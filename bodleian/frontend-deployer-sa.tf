40bfa4cf357733b8fb88c80c9bf0d1f850903b82
David Schmitz <davidschmitz@posteo.de>
2022-04-07 14:04:36 +0200

    Add frontend repository for bodleian

--- bodleian/frontend-deployer-sa.tf
+++ bodleian/frontend-deployer-sa.tf
@@ -1,24 +1,23 @@
 # This SA is used by the bodleian-service to deploy resources
 #tfsec:ignore:google-iam-no-privileged-service-accounts
-module "backend_deployer_sa" {
+module "frontend_deployer_sa" {
   source = "github.com/koenighotze/gcp-tf-modules/deployer-service-account"
 
-  name        = "bodleian-backend"
+  name        = "bodleian-frontend"
   project_id  = data.google_project.project.project_id
-  description = "Service account for CICD for the backend part"
+  description = "Service account for CICD for the frontend part"
 }
 
 #tfsec:ignore:google-iam-no-privileged-service-accounts tfsec:ignore:google-iam-no-project-level-service-account-impersonation
-resource "google_project_iam_member" "backend_deployer_sa_binding" {
+resource "google_project_iam_member" "frontend_deployer_sa_binding" {
   for_each = toset([
     "roles/iam.serviceAccountUser",
-    "roles/run.developer",
     "roles/viewer"
   ])
 
   project = data.google_project.project.project_id
   role    = each.key
-  member  = "serviceAccount:${module.backend_deployer_sa.email}"
+  member  = "serviceAccount:${module.frontend_deployer_sa.email}"
 }
 
 
--- bodleian/workload-identity-setup.tf
+++ bodleian/workload-identity-setup.tf
@@ -1,10 +1,20 @@
-resource "google_service_account_iam_binding" "workload_identity_sa_binding" {
+resource "google_service_account_iam_binding" "workload_identity_backend_sa_binding" {
   service_account_id = module.backend_deployer_sa.id
   role               = "roles/iam.workloadIdentityUser"
 
   members = [
     "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/koenighotze/infrastructure-${data.google_project.project.project_id}",
     "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/koenighotze/bodleian-service"
+  ]
+}
+
+resource "google_service_account_iam_binding" "workload_identity_frontend_sa_binding" {
+  service_account_id = module.frontend_deployer_sa.id
+  role               = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/koenighotze/infrastructure-${data.google_project.project.project_id}",
+    "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/koenighotze/bodleian-frontend"
   ]
 }
 
