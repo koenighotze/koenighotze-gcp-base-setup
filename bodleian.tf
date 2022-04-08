23f1dcc3068e36139989be10a5dc47a228b15ced
David Schmitz <davidschmitz@posteo.de>
2022-04-08 08:06:30 +0200

    Fixing SAs

--- bodleian.tf
+++ bodleian.tf
@@ -1,12 +1,12 @@
-module "bodleian" {
-  source = "./bodleian"
+# module "bodleian" {
+#   source = "./bodleian"
 
-  project_postfix = var.project_postfix
+#   project_postfix = var.project_postfix
 
-  github_admin_token              = var.github_admin_token
-  github_api_label_token          = var.github_api_label_token
-  workload_identity_provider_name = var.workload_identity_provider_name
-  workload_identity_pool_id       = var.workload_identity_pool_id
-  codacy_api_token                = var.codacy_api_token
-  container_registry              = module.platform.container_registry
-}
+#   github_admin_token              = var.github_admin_token
+#   github_api_label_token          = var.github_api_label_token
+#   workload_identity_provider_name = var.workload_identity_provider_name
+#   workload_identity_pool_id       = var.workload_identity_pool_id
+#   codacy_api_token                = var.codacy_api_token
+#   container_registry              = module.platform.container_registry
+# }
