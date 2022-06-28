37518570f1415e979c554a2eed3067124eaf4591
David Schmitz <davidschmitz@posteo.de>
2022-06-28 16:25:40 +0200

    Allow federation to create tokens for cicd SA

--- bodleian/workload-identity-setup.tf
+++ bodleian/workload-identity-setup.tf
@@ -9,3 +9,13 @@ resource "google_service_account_iam_binding" "workload_identity_sa_binding" {
   ]
 }
 
+resource "google_service_account_iam_binding" "workload_identity_sa_binding" {
+  service_account_id = module.backend_deployer_sa.id
+  role               = "roles/iam.serviceAccountTokenCreator"
+
+  members = [
+    "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/koenighotze/infrastructure-${data.google_project.project.project_id}",
+    "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/koenighotze/bodleian-service",
+    "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/koenighotze/bodleian-frontend"
+  ]
+}
