f809b79c100bc69eea4557fef12803e1e2a7c0d6
David Schmitz <davidschmitz@posteo.de>
2022-11-22 12:58:26 +0100

    Setup workload identity

--- bodleian/workload-identity-setup.tf
+++ bodleian/workload-identity-setup.tf
@@ -8,8 +8,20 @@ resource "google_service_account_iam_binding" "workload_identity_sa_binding" {
   role               = each.value
 
   members = [
-    "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/koenighotze/infrastructure-${data.google_project.project.project_id}",
-    "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/koenighotze/bodleian-service",
+    "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/koenighotze/bodleian-service"
+  ]
+}
+
+resource "google_service_account_iam_binding" "workload_identity_frontend_deployer_sa_binding" {
+  for_each = toset([
+    "roles/iam.workloadIdentityUser",
+    "roles/iam.serviceAccountTokenCreator"
+  ])
+
+  service_account_id = module.frontend_deployer_sa.id
+  role               = each.value
+
+  members = [
     "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/koenighotze/bodleian-frontend"
   ]
 }
