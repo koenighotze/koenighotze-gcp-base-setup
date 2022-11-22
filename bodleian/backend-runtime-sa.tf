0969cd64065b2b4c4b18e33a4634cddc5dcf974e
David Schmitz <davidschmitz@posteo.de>
2022-11-22 19:16:06 +0100

    WIP

--- bodleian/backend-runtime-sa.tf
+++ bodleian/backend-runtime-sa.tf
@@ -6,17 +6,17 @@ resource "google_service_account" "backend_service_sa" {
 
 # # This service must be able to run the backend as part of cloud run
 # #tfsec:ignore:google-iam-no-privileged-service-accounts
-resource "google_project_iam_member" "bodleian_backend_service_iam_binding" {
-  for_each = toset([
-    "roles/logging.logWriter",
-    "roles/iam.serviceAccountViewer",
-    "roles/run.developer",
-    "roles/viewer"
-  ])
-  project = data.google_project.project.project_id
-  role    = each.value
-  member  = "serviceAccount:${google_service_account.backend_service_sa.email}"
-}
+# resource "google_project_iam_member" "bodleian_backend_service_iam_binding" {
+#   for_each = toset([
+#     "roles/logging.logWriter",
+#     "roles/iam.serviceAccountViewer",
+#     "roles/run.developer",
+#     "roles/viewer"
+#   ])
+#   project = data.google_project.project.project_id
+#   role    = each.value
+#   member  = "serviceAccount:${google_service_account.backend_service_sa.email}"
+# }
 
 resource "google_service_account_iam_binding" "backend_service_sa_user_iam_binding_serviceaccountuser" {
   service_account_id = google_service_account.backend_service_sa.name
