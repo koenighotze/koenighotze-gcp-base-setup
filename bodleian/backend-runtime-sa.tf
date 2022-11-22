bf7976aeb8cb1c83ecdc5a5daed8505759e2fbf8
David Schmitz <davidschmitz@posteo.de>
2022-11-22 16:15:12 +0100

    WIP

--- bodleian/backend-runtime-sa.tf
+++ bodleian/backend-runtime-sa.tf
@@ -6,24 +6,24 @@ resource "google_service_account" "backend_service_sa" {
 
 # This service must be able to run the backend as part of cloud run
 #tfsec:ignore:google-iam-no-privileged-service-accounts
-# resource "google_project_iam_member" "bodleian_backend_service_iam_binding" {
-#   for_each = toset([
-#     "roles/logging.logWriter",
-#     "roles/iam.serviceAccountViewer",
-#     "roles/run.developer",
-#     "roles/viewer"
-#   ])
-#   project = data.google_project.project.project_id
-#   role    = each.value
-#   member  = "serviceAccount:${google_service_account.backend_service_sa.email}"
-# }
+resource "google_project_iam_member" "bodleian_backend_service_iam_binding" {
+  for_each = toset([
+    # "roles/logging.logWriter",
+    # "roles/iam.serviceAccountViewer",
+    # "roles/run.developer",
+    "roles/viewer"
+  ])
+  project = data.google_project.project.project_id
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.backend_service_sa.email}"
+}
 
-# resource "google_service_account_iam_binding" "backend_service_sa_user_iam_binding_serviceaccountuser" {
-#   service_account_id = google_service_account.backend_service_sa.name
-#   role               = "roles/iam.serviceAccountUser"
+resource "google_service_account_iam_binding" "backend_service_sa_user_iam_binding_serviceaccountuser" {
+  service_account_id = google_service_account.backend_service_sa.name
+  role               = "roles/iam.serviceAccountUser"
 
-#   members = [
-#     "serviceAccount:${module.backend_deployer_sa.email}",
-#     "serviceAccount:${google_service_account.bodleian_infrastructure_service_account.email}"
-#   ]
-# }
+  members = [
+    "serviceAccount:${module.backend_deployer_sa.email}",
+    "serviceAccount:${google_service_account.bodleian_infrastructure_service_account.email}"
+  ]
+}
