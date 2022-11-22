9b67fd52712b9509811f1b405e8f7d0c4267bcc6
David Schmitz <davidschmitz@posteo.de>
2022-11-22 14:35:43 +0100

    Add frontend runtime SA

--- bodleian/outputs.tf
+++ bodleian/outputs.tf
@@ -10,6 +10,14 @@ output "backend_deployer_sa_email" {
 #   value = google_service_account.backend_service_sa.email
 # }
 
+output "frontend_deployer_sa_email" {
+  value = module.frontend_deployer_sa.email
+}
+
+# output "frontend_service_sa_email" {
+#   value = google_service_account.frontend_service_sa.email
+# }
+
 output "state_bucket_url" {
   value = module.bodleian_project.state_bucket_url
 }
