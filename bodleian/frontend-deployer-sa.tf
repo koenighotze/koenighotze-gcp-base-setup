74ffd062061c35a9b5e010f696a47570e9a7bde8
David Schmitz <davidschmitz@posteo.de>
2022-11-22 17:59:11 +0100

    WIP

--- bodleian/frontend-deployer-sa.tf
+++ bodleian/frontend-deployer-sa.tf
@@ -1,25 +1,21 @@
-# # This SA is used by the bodleian-service to deploy resources
-# # tfsec:ignore:google-iam-no-privileged-service-accounts
-# module "frontend_deployer_sa" {
-#   source = "github.com/koenighotze/gcp-tf-modules/deployer-service-account"
+# This SA is used by the bodleian-service to deploy resources
+#tfsec:ignore:google-iam-no-privileged-service-accounts
+resource "google_service_account" "frontend_deployer_sa" {
+  project      = data.google_project.project.project_id
+  account_id   = "bodleian-frontend-cicd-tmp"
+  display_name = "bodleian-frontend"
+  description  = "CI/CD service account for ${data.google_project.project.project_id}"
+}
 
-#   name        = "bodleian-frontend"
-#   project_id  = data.google_project.project.project_id
-#   description = "Service account for CICD for the frontend part"
-# }
-
-# # tfsec:ignore:google-iam-no-privileged-service-accounts tfsec:ignore:google-iam-no-project-level-service-account-impersonation
-# resource "google_project_iam_member" "frontend_deployer_sa_binding" {
-#   for_each = toset([
-#     # "roles/iam.serviceAccountUser",
-#     "roles/run.developer",
-#     "roles/viewer"
-#   ])
-#   # TOKEN CREATOR
-
-#   project = data.google_project.project.project_id
-#   role    = each.key
-#   member  = "serviceAccount:${module.frontend_deployer_sa.email}"
-# }
-
-
+# This SA needs to be able to do some privileged work
+#tfsec:ignore:google-iam-no-privileged-service-accounts
+resource "google_project_iam_member" "iam_member_project" {
+  for_each = toset([
+    "roles/logging.logWriter",
+    "roles/run.developer",
+    "roles/viewer"
+  ])
+  project = data.google_project.project.project_id
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.frontend_deployer_sa.email}"
+}
--- bodleian/frontend-runtime-sa.tf
+++ bodleian/frontend-runtime-sa.tf
@@ -1,8 +1,8 @@
-# resource "google_service_account" "frontend_service_sa" {
-#   project     = data.google_project.project.project_id
-#   account_id  = "bodleian-frontend"
-#   description = "Service account for running the frontend part"
-# }
+resource "google_service_account" "frontend_service_sa" {
+  project     = data.google_project.project.project_id
+  account_id  = "bodleian-frontend"
+  description = "Service account for running the frontend part"
+}
 
 # # This service must be able to run the frontend as part of cloud run
 # #tfsec:ignore:google-iam-no-privileged-service-accounts
@@ -18,12 +18,12 @@
 #   member  = "serviceAccount:${google_service_account.frontend_service_sa.email}"
 # }
 
-# resource "google_service_account_iam_binding" "frontend_service_sa_user_iam_binding_serviceaccountuser" {
-#   service_account_id = google_service_account.frontend_service_sa.name
-#   role               = "roles/iam.serviceAccountUser"
+resource "google_service_account_iam_binding" "frontend_service_sa_user_iam_binding_serviceaccountuser" {
+  service_account_id = google_service_account.frontend_service_sa.name
+  role               = "roles/iam.serviceAccountUser"
 
-#   members = [
-#     "serviceAccount:${module.frontend_deployer_sa.email}",
-#     "serviceAccount:${google_service_account.bodleian_infrastructure_service_account.email}"
-#   ]
-# }
+  members = [
+    "serviceAccount:${module.frontend_deployer_sa.email}",
+    "serviceAccount:${google_service_account.bodleian_infrastructure_service_account.email}"
+  ]
+}
