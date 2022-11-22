e6b10ee25007778de99ed7b3689311c81ce34dba
David Schmitz <davidschmitz@posteo.de>
2022-11-22 21:55:17 +0100

    WIP

--- bodleian/frontend-runtime-sa.tf
+++ bodleian/frontend-runtime-sa.tf
@@ -18,12 +18,12 @@ resource "google_service_account" "frontend_service_sa" {
 #   member  = "serviceAccount:${google_service_account.frontend_service_sa.email}"
 # }
 
-# resource "google_service_account_iam_binding" "frontend_service_sa_user_iam_binding_serviceaccountuser" {
-#   service_account_id = google_service_account.frontend_service_sa.name
-#   role               = "roles/iam.serviceAccountUser"
+resource "google_service_account_iam_binding" "frontend_service_sa_user_iam_binding_serviceaccountuser" {
+  service_account_id = google_service_account.frontend_service_sa.name
+  role               = "roles/iam.serviceAccountUser"
 
-#   members = [
-#     "serviceAccount:${google_service_account.frontend_deployer_sa.email}",
-#     "serviceAccount:${google_service_account.bodleian_infrastructure_service_account.email}"
-#   ]
-# }
+  members = [
+    "serviceAccount:${google_service_account.frontend_deployer_sa.email}",
+    "serviceAccount:${google_service_account.bodleian_infrastructure_service_account.email}"
+  ]
+}
