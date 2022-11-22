cf64a31fdd2e33924037192b8479c7fd9feda805
David Schmitz <davidschmitz@posteo.de>
2022-11-22 14:27:44 +0100

    WIP

--- bodleian/outputs.tf
+++ bodleian/outputs.tf
@@ -6,9 +6,9 @@ output "backend_deployer_sa_email" {
   value = module.backend_deployer_sa.email
 }
 
-output "backend_service_sa_email" {
-  value = google_service_account.backend_service_sa.email
-}
+# output "backend_service_sa_email" {
+#   value = google_service_account.backend_service_sa.email
+# }
 
 output "state_bucket_url" {
   value = module.bodleian_project.state_bucket_url
