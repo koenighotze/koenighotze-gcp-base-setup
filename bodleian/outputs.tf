45dd4ab135049a7969c9f308ee2757a9c520ab26
David Schmitz <davidschmitz@posteo.de>
2022-11-23 08:56:53 +0100

    Add output

--- bodleian/outputs.tf
+++ bodleian/outputs.tf
@@ -2,21 +2,21 @@ output "infrastructure_sa_email" {
   value = google_service_account.bodleian_infrastructure_service_account.email
 }
 
-# output "backend_deployer_sa_email" {
-#   value = google_service_account.backend_deployer_sa.email
-# }
+output "backend_deployer_sa_email" {
+  value = google_service_account.backend_deployer_sa.email
+}
 
-# output "backend_service_sa_email" {
-#   value = google_service_account.backend_service_sa.email
-# }
+output "backend_service_sa_email" {
+  value = google_service_account.backend_service_sa.email
+}
 
-# output "frontend_deployer_sa_email" {
-#   value = module.frontend_deployer_sa.email
-# }
+output "frontend_deployer_sa_email" {
+  value = module.frontend_deployer_sa.email
+}
 
-# output "frontend_service_sa_email" {
-#   value = google_service_account.frontend_service_sa.email
-# }
+output "frontend_service_sa_email" {
+  value = google_service_account.frontend_service_sa.email
+}
 
 output "state_bucket_url" {
   value = module.bodleian_project.state_bucket_url
