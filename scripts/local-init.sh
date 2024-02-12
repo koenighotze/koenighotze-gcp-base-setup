754c28ff6a3517045f05bcf0a94f500106525908
koenighotze <david.schmitz.privat@gmail.com>
2024-02-12 11:59:48 +0100

    Update deps

---
+++ scripts/local-init.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# when a command fails, bash exits instead of continuing with the rest of the script
+set -o errexit
+# make the script fail, when accessing an unset variable
+set -o nounset
+# pipeline command is treated as failed, even if one command in the pipeline fails
+set -o pipefail
+# enable debug mode, by running your script as TRACE=1
+if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
+
+source "$(dirname "$0")/common.sh"
+
+# we assume gcloud to be downloaded and initialized
+# gcloud init
+# gcloud auth application-default login
+
+if bucket_exists "$SEED_PROJECT"; then
+    tf init -backend-config="bucket=$SEED_PROJECT"
+else
+    echo "Missing bucket '$SEED_PROJECT'; cannot initialize backend"
+    exit 1 
+fi
