8cb86bfdcee31e813fe1330ac76cb8ffa4438488
koenighotze <david.schmitz.privat@gmail.com>
2024-02-12 15:22:50 +0100

    WIP

--- seed/billing-alert.tf
+++ seed/billing-alert.tf
@@ -1,28 +1,28 @@
-resource "google_billing_budget" "overall_budget" {
-  billing_account = var.billing_account_id
-  display_name    = "Overall budget"
+# resource "google_billing_budget" "overall_budget" {
+#   billing_account = var.billing_account_id
+#   display_name    = "Overall budget"
 
-  amount {
-    specified_amount {
-      currency_code = "EUR"
-      units         = "20"
-    }
-  }
+#   amount {
+#     specified_amount {
+#       currency_code = "EUR"
+#       units         = "20"
+#     }
+#   }
 
-  threshold_rules {
-    threshold_percent = 0.5
-  }
+#   threshold_rules {
+#     threshold_percent = 0.5
+#   }
 
-  threshold_rules {
-    threshold_percent = 0.8
-  }
-}
-
-# resource "google_monitoring_notification_channel" "notification_channel" {
-#   display_name = "Example Notification Channel"
-#   type         = "email"
-
-#   labels = {
-#     email_address = "address@example.com"
+#   threshold_rules {
+#     threshold_percent = 0.8
 #   }
 # }
+
+# # resource "google_monitoring_notification_channel" "notification_channel" {
+# #   display_name = "Example Notification Channel"
+# #   type         = "email"
+
+# #   labels = {
+# #     email_address = "address@example.com"
+# #   }
+# # }
