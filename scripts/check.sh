b4cab9b454d4621f984c4b20f2c859d54d0812d8
koenighotze <david.schmitz.privat@gmail.com>
2024-02-14 16:56:56 +0100

    Setup tf examples project

--- scripts/check.sh
+++ scripts/check.sh
@@ -12,7 +12,7 @@ if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 source "$(dirname "$0")/common.sh"
 
 tflint --init
-tflint -f compact --disable-rule=terraform_module_pinned_source
+tflint -f compact --disable-rule=terraform_module_pinned_source --disable-rule=terraform_required_providers
 terraform validate -no-color
 terraform fmt -check -recursive
 
