661f4dbe8fa9edb332c56030ce800b04ad845b66
koenighotze <david.schmitz.privat@gmail.com>
2024-02-15 16:28:17 +0100

    WIP

--- terraform-examples/terraform-state-bucket.tf
+++ terraform-examples/terraform-state-bucket.tf
@@ -1,21 +1,21 @@
-resource "google_storage_bucket" "state_bucket" {
-  #checkov:skip=CKV_GCP_62:Logging deactivated for now
-  project                     = data.google_project.project.project_id
-  name                        = "${data.google_project.project.project_id}-state"
-  location                    = var.location
-  uniform_bucket_level_access = true
-  force_destroy               = true
+# resource "google_storage_bucket" "state_bucket" {
+#   #checkov:skip=CKV_GCP_62:Logging deactivated for now
+#   project                     = data.google_project.project.project_id
+#   name                        = "${data.google_project.project.project_id}-state"
+#   location                    = var.location
+#   uniform_bucket_level_access = true
+#   force_destroy               = true
 
-  public_access_prevention = "enforced"
+#   public_access_prevention = "enforced"
 
-  versioning {
-    #checkov:skip=CKV_GCP_78:We don't version states
-    enabled = false
-  }
-}
+#   versioning {
+#     #checkov:skip=CKV_GCP_78:We don't version states
+#     enabled = false
+#   }
+# }
 
-resource "google_storage_bucket_iam_member" "bucket_iam_member" {
-  bucket = google_storage_bucket.state_bucket.name
-  role   = "roles/storage.admin"
-  member = "serviceAccount:${google_service_account.sa.email}"
-}
+# resource "google_storage_bucket_iam_member" "bucket_iam_member" {
+#   bucket = google_storage_bucket.state_bucket.name
+#   role   = "roles/storage.admin"
+#   member = "serviceAccount:${google_service_account.sa.email}"
+# }
