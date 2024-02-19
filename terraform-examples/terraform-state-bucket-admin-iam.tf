93f5009d9f9b3f895f120c311c3a7968e2ccef29
koenighotze <david.schmitz.privat@gmail.com>
2024-02-19 14:24:02 +0100

    Setup state bucket for gcp training

--- terraform-examples/terraform-state-bucket-admin-iam.tf
+++ terraform-examples/terraform-state-bucket-admin-iam.tf
@@ -1,16 +1,16 @@
-# # This SA needs to be able to do some privileged work
-# #tfsec:ignore:google-iam-no-privileged-service-accounts
-# #checkov:skip=CKV_GCP_117:Allow admin for this bucket
-# resource "google_project_iam_binding" "project_iam_binding" {
-#   for_each = toset([
-#     "roles/storage.admin"
-#   ])
-#   project = data.google_project.project.project_id
-#   role    = "roles/storage.admin"
+# This SA needs to be able to do some privileged work
+#tfsec:ignore:google-iam-no-privileged-service-accounts
+#checkov:skip=CKV_GCP_117:Allow admin for this bucket
+resource "google_project_iam_binding" "project_iam_binding" {
+  for_each = toset([
+    "roles/storage.admin"
+  ])
+  project = data.google_project.project.project_id
+  role    = "roles/storage.admin"
 
-#   #tfsec:ignore:google-iam-no-privileged-service-accounts
-#   members = [
-#     "serviceAccount:${google_service_account.sa.email}",
-#     "serviceAccount:${var.admin_sa_email}"
-#   ]
-# }
+  #tfsec:ignore:google-iam-no-privileged-service-accounts
+  members = [
+    "serviceAccount:${google_service_account.sa.email}",
+    "serviceAccount:${var.admin_sa_email}"
+  ]
+}
