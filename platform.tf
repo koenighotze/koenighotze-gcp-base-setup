dde3f4177eb0a173757d6679943007319ad87bbe
David Schmitz <david.schmitz.privat@gmail.com>
2024-05-17 10:09:08 +0200

    Cleanup platform

--- platform.tf
+++ platform.tf
@@ -1,6 +1,5 @@
 module "platform" {
   source = "./platform"
 
-  admin_sa_email  = var.seed_sa_email
   project_postfix = var.project_postfix
 }
--- platform/sa.tf
+++ platform/sa.tf
@@ -5,19 +5,6 @@ resource "google_service_account" "sa" {
   description = "Admin service account for ${data.google_project.platform_project.project_id}"
 }
 
-# # This SA needs to be able to do some privileged work
-# resource "google_project_iam_member" "iam_member_project" {
-#   #checkov:skip=CKV_GCP_117:Allow
-#   for_each = toset([
-#     "roles/logging.logWriter",
-#     "roles/run.developer",
-#     "roles/viewer"
-#   ])
-#   project = data.google_project.project.project_id
-#   role    = each.value
-#   member  = "serviceAccount:${google_service_account.backend_deployer_sa.email}"
-# }
-
 # This SA needs to be able to do some privileged work
 resource "google_project_iam_member" "seed_platform_iam_member_project" {
   for_each = toset([
