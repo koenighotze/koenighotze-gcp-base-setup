e8f038321e06ba59a27f3e565834457036b149cf
David Schmitz <david.schmitz.privat@gmail.com>
2024-05-29 12:31:15 +0200

    Allow public repos and google keys

--- terraform-examples/terraform-state-bucket-admin-iam.tf
+++ terraform-examples/terraform-state-bucket-admin-iam.tf
@@ -1,5 +1,6 @@
 # This SA needs to be able to do some privileged work
 #checkov:skip=CKV_GCP_117:Allow admin for this bucket
+#trivy:ignore:avd-gcp-0007
 resource "google_project_iam_binding" "project_iam_binding" {
   for_each = toset([
     "roles/storage.admin"
@@ -7,7 +8,6 @@ resource "google_project_iam_binding" "project_iam_binding" {
   project = data.google_project.project.project_id
   role    = "roles/storage.admin"
 
-  #trivy:ignore:avd-gcp-0007
   members = [
     "serviceAccount:${google_service_account.sa.email}",
     "serviceAccount:${var.admin_sa_email}"
