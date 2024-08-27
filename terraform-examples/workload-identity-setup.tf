f917cdccb5d7c95992de6efc401a80709c955a82
David Schmitz <david.schmitz.privat@gmail.com>
2024-08-27 11:16:17 +0200

    debugging

--- terraform-examples/workload-identity-setup.tf
+++ terraform-examples/workload-identity-setup.tf
@@ -2,24 +2,35 @@ locals {
   member = "principalSet://iam.googleapis.com/projects/${var.seed_project_id}/locations/global/workloadIdentityPools/${var.workload_identity_pool_id}/attribute.repository/${data.github_repository.repository.full_name}"
 }
 
-resource "google_service_account_iam_binding" "workload_identity_sa_binding" {
-  for_each = toset([
-    "roles/iam.workloadIdentityUser",
-    "roles/iam.serviceAccountTokenCreator"
-  ])
+# resource "google_service_account_iam_binding" "workload_identity_sa_binding" {
+#   for_each = toset([
+#     "roles/iam.workloadIdentityUser",
+#     "roles/iam.serviceAccountTokenCreator"
+#   ])
 
-  service_account_id = google_service_account.sa.id
-  role               = each.value
+#   service_account_id = google_service_account.sa.id
+#   role               = each.value
 
-  members = [
-    local.member
-  ]
-}
+#   members = [
+#     local.member
+#   ]
+# }
 
-# Allow repository to use deployer service account
-resource "google_service_account_iam_member" "gce-default-workload_identity_sa_member" {
+# # Allow repository to use deployer service account
+# resource "google_service_account_iam_member" "gce-default-workload_identity_sa_member" {
+#   service_account_id = google_service_account.sa.id
+#   role               = "roles/iam.workloadIdentityUser"
+#   member             = local.member
+# }
+
+resource "google_service_account_iam_member" "workload_identity_sa_member-a" {
   service_account_id = google_service_account.sa.id
   role               = "roles/iam.workloadIdentityUser"
   member             = local.member
 }
 
+resource "google_service_account_iam_member" "workload_identity_sa_member-b" {
+  service_account_id = google_service_account.sa.id
+  role               = "roles/iam.serviceAccountTokenCreator"
+  member             = local.member
+}
