e2c42c8f6e1d016d3e3de843c8feedd37ddb90ab
David Schmitz <david.schmitz.privat@gmail.com>
2024-08-27 07:36:19 +0200

    Debugging

--- bodleian.tf
+++ bodleian.tf
@@ -1,11 +1,11 @@
-module "bodleian" {
-  source = "./bodleian"
+# module "bodleian" {
+#   source = "./bodleian"
 
-  project_id = local.bodleian_project_id
+#   project_id = local.bodleian_project_id
 
-  github_admin_token              = var.github_admin_token
-  workload_identity_provider_name = var.workload_identity_provider_name
-  workload_identity_pool_id       = var.workload_identity_pool_id
-  codacy_api_token                = var.codacy_api_token
-  container_registry              = module.platform.container_registry
-}
+#   github_admin_token              = var.github_admin_token
+#   workload_identity_provider_name = var.workload_identity_provider_name
+#   workload_identity_pool_id       = var.workload_identity_pool_id
+#   codacy_api_token                = var.codacy_api_token
+#   container_registry              = module.platform.container_registry
+# }
