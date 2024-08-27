278cd45b7413bcbd8131ef6c6efb33c834fe6eca
David Schmitz <david.schmitz.privat@gmail.com>
2024-08-27 10:51:51 +0200

    Cleanup lint errors in sh

--- scripts/setup-workload-identity.sh
+++ scripts/setup-workload-identity.sh
@@ -11,140 +11,144 @@ if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
 source "$(dirname "$0")/common.sh"
 
-# function workload_identity_pool_exists() {
-#   local project_id=$1
-#   local workload_identity_pool="$2"
+echo "TODO: *** THIS IS BEING MOVED TO TERRAFORM"
 
-#   local existing_pool
-#   existing_pool=$(
-#     gcloud iam workload-identity-pools describe "$workload_identity_pool" \
-#       --project="$project_id" \
-#       --location="global" \
-#       --format="value(name)" 2>/dev/null
-#   )
+function workload_identity_pool_exists() {
+  local project_id=$1
+  local workload_identity_pool="$2"
 
-#   if [ -z "$existing_pool" ]
-#   then
-#     return 1
-#   else
-#     return 0
-#   fi
-# }
+  local existing_pool
+  existing_pool=$(
+    gcloud iam workload-identity-pools describe "$workload_identity_pool" \
+      --project="$project_id" \
+      --location="global" \
+      --format="value(name)" 2>/dev/null
+  )
 
-# function setup_workload_pool() {
-#   local project_id="$1"
-#   local workload_identity_pool="$2"
+  if [ -z "$existing_pool" ]
+  then
+    return 1
+  else
+    return 0
+  fi
+}
 
-#   if workload_identity_pool_exists "$project_id" "$workload_identity_pool";
-#   then
-#     echo "Workload identity pool $workload_identity_pool already exists in project $project_id"
-#     return
-#   fi
+function setup_workload_pool() {
+  local project_id="$1"
+  local workload_identity_pool="$2"
 
-#   echo "Creating workload identity pool $workload_identity_pool in project $project_id"
+  if workload_identity_pool_exists "$project_id" "$workload_identity_pool";
+  then
+    echo "Workload identity pool $workload_identity_pool already exists in project $project_id"
+    return
+  fi
 
-#   gcloud iam workload-identity-pools create "$workload_identity_pool" \
-#           --project="$project_id" \
-#           --location="global" \
-#           --display-name="WIP for GitHub CI/CD"
-# }
+  echo "Creating workload identity pool $workload_identity_pool in project $project_id"
 
-# function fetch_workload_identity_pool_id() {
-#   local workload_identity_pool="$1"
+  gcloud iam workload-identity-pools create "$workload_identity_pool" \
+          --project="$project_id" \
+          --location="global" \
+          --display-name="WIP for GitHub CI/CD"
+}
 
-#   gcloud iam workload-identity-pools describe "$workload_identity_pool" \
-#     --location="global" \
-#     --format="value(name)"
-# }
+function fetch_workload_identity_pool_id() {
+  local workload_identity_pool="$1"
 
-# function fetch_workload_identity_pool_provider_id() {
-#   local provider_id="$1"
-#   local workload_identity_pool="$2"
+  gcloud iam workload-identity-pools describe "$workload_identity_pool" \
+    --location="global" \
+    --format="value(name)"
+}
 
-#   gcloud iam workload-identity-pools providers describe "$provider_id" \
-#     --workload-identity-pool="$workload_identity_pool" \
-#     --location="global" \
-#     --format="value(name)"
-# }
+function fetch_workload_identity_pool_provider_id() {
+  local provider_id="$1"
+  local workload_identity_pool="$2"
 
-# function workload_identity_pool_provider_exists() {
-#   local provider_id="$1"
-#   local workload_identity_pool="$2"
+  gcloud iam workload-identity-pools providers describe "$provider_id" \
+    --workload-identity-pool="$workload_identity_pool" \
+    --location="global" \
+    --format="value(name)"
+}
 
-#   local existing_provider
-#   existing_provider=$(
-#     gcloud iam workload-identity-pools providers describe "$provider_id" \
-#       --workload-identity-pool="$workload_identity_pool" \
-#       --location="global" \
-#       --format="value(name)" 2>/dev/null
-#   )
+function workload_identity_pool_provider_exists() {
+  local provider_id="$1"
+  local workload_identity_pool="$2"
 
-#   if [ -z "$existing_provider" ]
-#   then
-#     return 1
-#   else
-#     return 0
-#   fi
-# }
+  local existing_provider
+  existing_provider=$(
+    gcloud iam workload-identity-pools providers describe "$provider_id" \
+      --workload-identity-pool="$workload_identity_pool" \
+      --location="global" \
+      --format="value(name)" 2>/dev/null
+  )
 
-# function link_github_oidc_to_workload_pool() {
-#   local provider_id="$1"
-#   local workload_identity_pool="$2"
+  if [ -z "$existing_provider" ]
+  then
+    return 1
+  else
+    return 0
+  fi
+}
 
-#   if workload_identity_pool_provider_exists "$provider_id" "$workload_identity_pool";
-#   then
-#     echo "GitHub OIDC provider already linked to workload identity pool $workload_identity_pool"
-#     return
-#   fi
+function link_github_oidc_to_workload_pool() {
+  local provider_id="$1"
+  local workload_identity_pool="$2"
 
-#   echo "Linking GitHub OIDC provider to workload identity pool $workload_identity_pool"
+  echo "Linking provider $provider_id to workload identity pool $workload_identity_pool"
 
-#   gcloud iam workload-identity-pools providers create-oidc "$provider_id" \
-#           --location="global" \
-#           --workload-identity-pool="$workload_identity_pool"  \
-#           --display-name="GitHub OIDC provider" \
-#           --attribute-mapping="google.subject=assertion.sub,attribute.actor=assertion.actor,attribute.repository=assertion.repository" \
-#           --issuer-uri="https://token.actions.githubusercontent.com"
-# }
+  if workload_identity_pool_provider_exists "$provider_id" "$workload_identity_pool";
+  then
+    echo "GitHub OIDC provider already linked to workload identity pool $workload_identity_pool"
+    return
+  fi
 
-# function allow_seed_repository_access_to_the_seed_account() {
-#   local sa_email="$1"
-#   local workload_identity_pool_id="$2"
-#   local repository="$3"
+  gcloud iam workload-identity-pools providers create-oidc "$provider_id" \
+          --location="global" \
+          --workload-identity-pool="$workload_identity_pool"  \
+          --display-name="GitHub OIDC provider" \
+          --attribute-mapping="google.subject=assertion.sub,attribute.actor=assertion.actor,attribute.repository=assertion.repository" \
+          --issuer-uri="https://token.actions.githubusercontent.com"
+}
 
-#   echo "Allowing seed repository $repository to access the seed service account $sa_email"
+function allow_seed_repository_access_to_the_seed_account() {
+  local sa_email="$1"
+  local workload_identity_pool_id="$2"
+  local repository="$3"
 
-#   gcloud iam service-accounts add-iam-policy-binding "$sa_email" \
-#       --role="roles/iam.workloadIdentityUser" \
-#       --member="principalSet://iam.googleapis.com/${workload_identity_pool_id}/attribute.repository/${repository}"
-# }
+  echo "Allowing seed repository $repository to access the seed service account $sa_email"
 
-# function setup_github_secrets() {
-#   local workload_identity_provider="$1"
-#   local workload_identity_pool_id="$2"
-#   local repository="$3"
+  gcloud iam service-accounts add-iam-policy-binding "$sa_email" \
+      --role="roles/iam.workloadIdentityUser" \
+      --member="principalSet://iam.googleapis.com/${workload_identity_pool_id}/attribute.repository/${repository}"
+}
 
-#   echo "Setting up GitHub secrets for repository $repository"
+function setup_github_secrets() {
+  local workload_identity_provider="$1"
+  local workload_identity_pool_id="$2"
+  local repository="$3"
 
-#   gh secret set WORKLOAD_IDENTITY_PROVIDER -R "$repository" -b "$workload_identity_provider"
-#   gh secret set WORKLOAD_IDENTITY_POOL_ID -R "$repository" -b "$workload_identity_pool_id"
-# }
+  echo "Setting up GitHub secrets for repository $repository"
 
-# function main() {
-#   # shellcheck disable=SC2153
-#   setup_workload_pool "$SEED_PROJECT" "$WORKLOAD_IDENTITY_POOL"
+  gh secret set WORKLOAD_IDENTITY_PROVIDER -R "$repository" -b "$workload_identity_provider"
+  gh secret set WORKLOAD_IDENTITY_POOL_ID -R "$repository" -b "$workload_identity_pool_id"
+}
 
-#   workload_identity_pool_id=$(fetch_workload_identity_pool_id "$WORKLOAD_IDENTITY_POOL")
-#   workload_identity_pool_provider_id=$(fetch_workload_identity_pool_provider_id "$PROVIDER_ID" "$WORKLOAD_IDENTITY_POOL")
+function main() {
+  echo "Setting up workload identity for project $SEED_PROJECT and identity pool $WORKLOAD_IDENTITY_POOL"
 
-#   # shellcheck disable=SC2153
-#   link_github_oidc_to_workload_pool "$PROVIDER_ID" "$WORKLOAD_IDENTITY_POOL"
+  # shellcheck disable=SC2153
+  setup_workload_pool "$SEED_PROJECT" "$WORKLOAD_IDENTITY_POOL"
 
-#   # shellcheck disable=SC2153
-#   allow_seed_repository_access_to_the_seed_account "$SA_EMAIL" "$workload_identity_pool_id" "$SEED_REPOSITORY"
+  workload_identity_pool_id=$(fetch_workload_identity_pool_id "$WORKLOAD_IDENTITY_POOL")
+  workload_identity_pool_provider_id=$(fetch_workload_identity_pool_provider_id "$PROVIDER_ID" "$WORKLOAD_IDENTITY_POOL")
 
-#   setup_github_secrets "$workload_identity_pool_provider_id" "$workload_identity_pool_id" "$SEED_REPOSITORY"
-# }
+  # shellcheck disable=SC2153
+  link_github_oidc_to_workload_pool "$PROVIDER_ID" "$WORKLOAD_IDENTITY_POOL"
+
+  # shellcheck disable=SC2153
+  allow_seed_repository_access_to_the_seed_account "$SA_EMAIL" "$workload_identity_pool_id" "$SEED_REPOSITORY"
+
+  setup_github_secrets "$workload_identity_pool_provider_id" "$workload_identity_pool_id" "$SEED_REPOSITORY"
+}
 
 main "$@"
 
